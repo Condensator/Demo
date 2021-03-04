f985c9365bfc420f4492cce73da523990cd234c1
Condensator <6766063c69b6399ea08f144db66b600524bc9753>
2021-03-04 15:27:33 +0200

    commit something

--- DEV/DEV_ENV_1/Database 1/dbo/Table/Table_1/Script.sql
+++ DEV/DEV_ENV_1/Database 1/dbo/Table/Table_1/Script.sql
@@ -1,5 +1,8 @@
 CREATE TABLE [dbo].[Table_1] (
-    [asd] NCHAR (10) NOT NULL
+    [asd]          NCHAR (10) NULL,
+    [qwe]          NCHAR (10) NULL,
+    [zxcz]         NCHAR (10) NULL,
+    [sdfgdfsgsdfg] NCHAR (10) NULL
 );
 
 
