c0a7fde7bab45171f89ad5fcec74fb31e35964e9
dbmsasha <015f7af3a0c0a1df5ad1240653e05205a8e9ba7a>
2021-03-07 16:54:44 +0200

    commit?

--- DEV/DEV_ENV_1/Database 1/dbo/Table/Table_1/Script.sql
+++ DEV/DEV_ENV_1/Database 1/dbo/Table/Table_1/Script.sql
@@ -1,8 +1,5 @@
 CREATE TABLE [dbo].[Table_1] (
-    [asd]          NCHAR (10) NULL,
-    [qwe]          NCHAR (10) NULL,
-    [zxcz]         NCHAR (10) NULL,
-    [sdfgdfsgsdfg] NCHAR (10) NULL
+    [asd] NCHAR (10) NULL
 );
 
 
